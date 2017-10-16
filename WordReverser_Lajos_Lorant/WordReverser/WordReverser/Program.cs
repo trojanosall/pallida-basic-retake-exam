e82a2ca6b3fb7436706b87863796e72ee4c5c3fb
Lorant Lajos <trojanosall@gmail.com>
2017-10-16 10:52:06 +0200

    Basic retake exam - Word Reverser - Two funcion created but the workin is still progress

---
+++ WordReverser_Lajos_Lorant/WordReverser/WordReverser/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordReverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a sentence as an input, reverses the letters in every word (but not reverses the words order in the sentence), and returns the sentence with the reversed words.
+            // You shouldn't care about upper or lower case letters.
+            // Example:
+            //
+            // string reversedWords = WordReverser("lleW ,enod taht saw ton taht drah");
+            // Console.WriteLine(reversedWords);
+            //
+            // Should print:
+            // Well done, that was not that hard.
+
+            string sentence = "lleW ,enod taht saw ton taht drah";
+
+            string reversedWords = WordReverser(sentence);
+        }
+
+        public static string WordReverser(string sentenceForFuncion)
+        {
+            string[] words = sentenceForFuncion.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+
+
+
+            return reversedSentece;
+        }
+
+    }
+}
