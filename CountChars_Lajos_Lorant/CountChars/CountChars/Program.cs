09064e6ae33e899868206ec68e18ea452e1a75a6
Lorant Lajos <trojanosall@gmail.com>
2017-10-16 09:46:09 +0200

    Basic retake exam - CountChars - Funcion + Dictionary created - before the print out of result

--- CountChars_Lajos_Lorant/CountChars/CountChars/Program.cs
+++ CountChars_Lajos_Lorant/CountChars/CountChars/Program.cs
@@ -30,13 +30,41 @@ namespace CountChars
 
             CountChars(fileName);
 
-
+            // var countedLetters = CountLetters("da_vinci_code.txt");
+            // Console.WriteLine("0 occured {0} times in the file.", countedLetters["0"]);
+            // Console.WriteLine("1 occured {0} times in the file.", countedLetters["1"]);
+            // Console.WriteLine("x occured {0} times in the file.", countedLetters["x"]);
 
         }
 
         public static Dictionary<string, int> CountChars(string fileNameForFuncion)
         {
+            string[] myArray = File.ReadAllLines(fileNameForFuncion);
+            Array.Sort(myArray);
 
+            var myDictionary = new Dictionary<string, int>();
+
+            int numberOfZero = 0, numberOfOne = 0, numberOfX = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] == "0")
+                {
+                    numberOfZero++;
+                }
+                else if (myArray[i] == "1")
+                {
+                    numberOfOne++;
+                }
+                else
+                {
+                    numberOfX++;
+                }
+            }
+
+            myDictionary.Add("0", numberOfZero);
+            myDictionary.Add("1", numberOfOne);
+            myDictionary.Add("x", numberOfX);
 
             return myDictionary;
         }
